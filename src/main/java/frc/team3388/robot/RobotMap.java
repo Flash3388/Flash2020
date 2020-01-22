b678c4c303e58c2e7bb93f31274bc276f45da6e8
Tom Tzook <tomtzook@gmail.com>
2020-01-22 17:48:11 +0200

    climb system

--- src/main/java/frc/team3388/robot/RobotMap.java
+++ src/main/java/frc/team3388/robot/RobotMap.java
@@ -3,4 +3,5 @@ package frc.team3388.robot;
 public class RobotMap {
     public static final int CLIMB_CONTROLLER_PULL = 0;
     public static final int CLIMB_CONTROLLER_RISE = 1;
+    public static final int CLIMB_XBOX_CONTROLLER = 0;
 }
