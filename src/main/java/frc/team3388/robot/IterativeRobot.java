dcd04c838e4d58214a00042f7e3a52e965879724
flash 3388 <flash@Flash3388.banana>
2020-01-22 18:53:49 +0200

    update

--- src/main/java/frc/team3388/robot/IterativeRobot.java
+++ src/main/java/frc/team3388/robot/IterativeRobot.java
@@ -1,14 +1,13 @@
 package frc.team3388.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.flash3388.flashlib.frc.robot.IterativeRobotInterface;
 import com.flash3388.flashlib.robot.Robot;
 import com.flash3388.flashlib.robot.hid.xbox.XboxController;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.AnalogGyro;
+import com.flash3388.flashlib.robot.scheduling.actions.Actions;
+import com.flash3388.flashlib.time.Time;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.SPI;
 import frc.team3388.robot.actions.turret.TurretRotateLeft;
 import frc.team3388.robot.actions.turret.TurretRotateRight;
 import frc.team3388.robot.subsystems.TurretSystem;
@@ -21,11 +20,13 @@ public class IterativeRobot implements IterativeRobotInterface {
 
     public IterativeRobot(Robot robot) {
         mRobot = robot;
-        mTurretSystem = new TurretSystem(new PWMVictorSPX(RobotMap.TURRET_CONTROLLER) , new AnalogGyro(RobotMap.TURRET_GYRO));
+        mTurretSystem = new TurretSystem(new PWMVictorSPX(RobotMap.TURRET_CONTROLLER) , new ADXRS450_Gyro(SPI.Port.kOnboardCS0));
         mController = new XboxController(0);
 
         mController.getDPad().getRight().whileHeld(new TurretRotateRight(mTurretSystem));
         mController.getDPad().getLeft().whileHeld(new TurretRotateLeft(mTurretSystem));
+
+
     }
 
     @Override
@@ -39,7 +40,8 @@ public class IterativeRobot implements IterativeRobotInterface {
 
     @Override
     public void teleopInit() {
-
+        Actions.periodic(()->System.out.println(mTurretSystem.getAngle()), Time.seconds(1))
+                .start();
     }
 
     @Override
@@ -65,6 +67,5 @@ public class IterativeRobot implements IterativeRobotInterface {
 
     @Override
     public void robotPeriodic() {
-        NetworkTableInstance.getDefault().getTable("test").getEntry("gyro").setDouble(mTurretSystem.getAngle());
     }
 }
