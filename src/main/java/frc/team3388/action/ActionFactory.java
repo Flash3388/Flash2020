ffb39fae4b60468ef098d40fb421250bf0a51766
Quacking duck <danielmkhlv@gmail.com>
2020-02-09 04:47:30 +0200

    added requiers to all the actions in action factory

--- src/main/java/frc/team3388/action/ActionFactory.java
+++ src/main/java/frc/team3388/action/ActionFactory.java
@@ -8,20 +8,20 @@ public class ActionFactory {
     public static Action intakeAction(IntakeSystem intakeSystem) {
         return Actions.sequential(
                 Actions.runnableAction(intakeSystem::intake),
-                Actions.instantAction(intakeSystem::stop));
+                Actions.instantAction(intakeSystem::stop)).requires(intakeSystem);
     }
 
     public static Action unfoldIntakeAction(IntakeSystem intakeSystem) {
-        return Actions.instantAction(intakeSystem::unfold);
+        return Actions.instantAction(intakeSystem::unfold).requires(intakeSystem);
     }
 
     public static Action foldIntakeAction(IntakeSystem intakeSystem) {
-        return Actions.instantAction(intakeSystem::fold);
+        return Actions.instantAction(intakeSystem::fold).requires(intakeSystem);
     }
 
     public static Action engageIntakeAction(IntakeSystem intakeSystem) {
         return Actions.sequential(
                 unfoldIntakeAction(intakeSystem),
-                intakeAction(intakeSystem));
+                intakeAction(intakeSystem)).requires(intakeSystem);
     }
 }
