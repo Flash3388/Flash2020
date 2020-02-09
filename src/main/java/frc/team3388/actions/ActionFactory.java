e8b27bbd504bdd3cc879d7e65bd57d5a90f3fb35
Quacking duck <danielmkhlv@gmail.com>
2020-02-09 17:29:11 +0200

    changed some methods in action factory

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -2,7 +2,6 @@ package frc.team3388.actions;
 
 import com.flash3388.flashlib.robot.scheduling.actions.Action;
 import com.flash3388.flashlib.robot.scheduling.actions.Actions;
-import frc.team3388.subsystems.TurretSystem;
 
 import java.util.function.DoubleSupplier;
 
@@ -10,18 +9,23 @@ public class ActionFactory {
     public static Action keepTurretAtAngle(TurretSystem turretSystem, DoubleSupplier targetSupplier) {
         return Actions.sequential(
                 Actions.instantAction(turretSystem::resetPidController),
-                Actions.runnableAction(() -> turretSystem.rotateTo(targetSupplier.getAsDouble()))
+                Actions.runnableAction(() -> turretSystem.rotateTo(targetSupplier.getAsDouble())),
+                Actions.instantAction(turretSystem::stop)
         ).requires(turretSystem);
     }
 
     public static Action manualTurretRotateAction(TurretSystem turretSystem, DoubleSupplier rotateValueSupplier) {
         final double TURRET_ROTATE_MODIFIER = 0.5;
-        return Actions.runnableAction(() -> turretSystem.rotate(rotateValueSupplier.getAsDouble() * TURRET_ROTATE_MODIFIER)).requires(turretSystem);
+        return Actions.sequential(
+                Actions.runnableAction(() -> turretSystem.rotate(rotateValueSupplier.getAsDouble() * TURRET_ROTATE_MODIFIER)),
+                Actions.instantAction(turretSystem::stop)
+        ).requires(turretSystem);
     }
 
     public static Action roughTurretRotateAction(TurretSystem turretSystem, double target) {
         return Actions.conditional(() -> turretSystem.hasReachedTarget(target),
                 Actions.sequential(
+                        Actions.instantAction(turretSystem::resetPidController),
                         Actions.runnableAction(() -> turretSystem.rotateTo(target)),
                         Actions.instantAction(turretSystem::stop))
         ).requires(turretSystem);
