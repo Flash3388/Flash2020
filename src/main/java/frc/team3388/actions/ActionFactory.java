624c0ea98aa2e45500012810355e688c7fd8f832
Quacking duck <danielmkhlv@gmail.com>
2020-02-09 05:39:33 +0200

    some refactoring in action factory

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -7,28 +7,32 @@ import frc.team3388.subsystems.storage.ShooterFeederSystem;
 import frc.team3388.subsystems.storage.StorageSystem;
 
 public class ActionFactory {
-    public static Action feedShooterAction(ShooterFeederSystem feederSystem) {
-        return Actions.sequential(
-                Actions.runnableAction(feederSystem::feed),
-                Actions.instantAction(feederSystem::stop)).requires(feederSystem);
-    }
-
-    public static Action moveStorageAction(StorageSystem storageSystem) {
-        return Actions.sequential(
-                Actions.runnableAction(storageSystem::move),
-                Actions.instantAction(storageSystem::stop)).requires(storageSystem);
-    }
-
     public static Action fullFeedAction(ShooterFeederSystem feederSystem, StorageSystem storageSystem) {
         return Actions.parallel(
                 feedShooterAction(feederSystem),
-                moveStorageAction(storageSystem)).requires(storageSystem, feederSystem);
+                moveStorageAction(storageSystem)
+        ).requires(storageSystem, feederSystem);
     }
 
     public static Action fullFeedActionUntilEmpty(ShooterFeederSystem feederSystem, StorageSystem storageSystem, BallCountingSystem countingSystem) {
         return Actions.conditional(countingSystem::isEmpty,
                 Actions.parallel(
                         feedShooterAction(feederSystem),
-                        moveStorageAction(storageSystem))).requires(storageSystem, feederSystem, countingSystem);
+                        moveStorageAction(storageSystem))
+        ).requires(storageSystem, feederSystem, countingSystem);
+    }
+
+    public static Action feedShooterAction(ShooterFeederSystem feederSystem) {
+        return Actions.sequential(
+                Actions.runnableAction(feederSystem::feed),
+                Actions.instantAction(feederSystem::stop)
+        ).requires(feederSystem);
+    }
+
+    public static Action moveStorageAction(StorageSystem storageSystem) {
+        return Actions.sequential(
+                Actions.runnableAction(storageSystem::move),
+                Actions.instantAction(storageSystem::stop)
+        ).requires(storageSystem);
     }
 }
