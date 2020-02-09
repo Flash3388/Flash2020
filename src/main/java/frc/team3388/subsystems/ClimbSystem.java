d8b492cdebf52047dbbbf4180872eada676eadb3
Quacking duck <danielmkhlv@gmail.com>
2020-02-09 05:23:42 +0200

    added factory method

--- src/main/java/frc/team3388/subsystems/ClimbSystem.java
+++ src/main/java/frc/team3388/subsystems/ClimbSystem.java
@@ -1,9 +1,12 @@
 package frc.team3388.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.flash3388.flashlib.robot.scheduling.Subsystem;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class ClimbSystem extends Subsystem {
+    private static final int LIFT_CONTROLLER_PORT = 4;
+    private static final int HOOK_CONTROLLER_PORT = 5;
     private static final double HOOK_LIFT_SPEED = 0.3;
     private static final double LIFT_SPEED = 1;
 
@@ -15,12 +18,15 @@ public class ClimbSystem extends Subsystem {
         this.hookController = hookController;
     }
 
-    public void lift() {
-        liftController.set(LIFT_SPEED);
+    public static ClimbSystem forRobot() {
+        WPI_TalonSRX liftController = new WPI_TalonSRX(LIFT_CONTROLLER_PORT);
+        WPI_TalonSRX hookController = new WPI_TalonSRX(HOOK_CONTROLLER_PORT);
+
+        return new ClimbSystem(liftController, hookController);
     }
 
-    public void lower() {
-        liftController.set(-LIFT_SPEED);
+    public void lift() {
+        liftController.set(LIFT_SPEED);
     }
 
     public void liftHook() {
