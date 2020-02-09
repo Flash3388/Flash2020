8a6a7644deed4b890df67050c168c63a7ed3e3b0
Quacking duck <danielmkhlv@gmail.com>
2020-02-09 04:41:07 +0200

    optimized robot classed

--- src/main/java/frc/team3388/robot/IterativeRobot.java
+++ src/main/java/frc/team3388/robot/IterativeRobot.java
@@ -1,26 +1,13 @@
 package frc.team3388.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.flash3388.flashlib.frc.robot.IterativeRobotInterface;
 import com.flash3388.flashlib.robot.Robot;
-import com.flash3388.flashlib.robot.hid.xbox.XboxButton;
-import com.flash3388.flashlib.robot.hid.xbox.XboxController;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import frc.team3388.action.EngageIntake;
-import frc.team3388.subsystems.IntakeSystem;
 
 public class IterativeRobot implements IterativeRobotInterface {
-    private final Robot mRobot;
-    private  final IntakeSystem intakeSystem;
-    private  final XboxController xboxController;
+    private final Robot robot;
 
     public IterativeRobot(Robot robot) {
-        mRobot = robot;
-        this.intakeSystem=new IntakeSystem(new  WPI_TalonSRX (RobotMap.INTAKE_MANOA), 
-                new DoubleSolenoid(RobotMap.INTAKE_PISTON_FORWARD, RobotMap.INTAKE_PISTON_REVERSE),
-                rightPiston, new DoubleSolenoid(RobotMap.INTAKE_PISTON1_FORWARD,RobotMap.INTAKE_PISTON1_REVERSE));
-        xboxController =new XboxController(RobotMap.CONTROLLER);
-        xboxController.getButton(XboxButton.A).whenPressed(new EngageIntake(intakeSystem));
+        this.robot = robot;
     }
 
     @Override
