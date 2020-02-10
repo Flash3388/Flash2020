788d5a70819cb55e629ee08bafc6337ee8630a71
Quacking duck <danielmkhlv@gmail.com>
2020-02-10 04:35:07 +0200

    removed unnecessary triggers

--- src/main/java/frc/team3388/subsystems/storage/BallCountingSystem.java
+++ src/main/java/frc/team3388/subsystems/storage/BallCountingSystem.java
@@ -1,6 +1,7 @@
 package frc.team3388.subsystems.storage;
 
 import com.flash3388.flashlib.robot.scheduling.Subsystem;
+import com.flash3388.flashlib.robot.scheduling.actions.Action;
 import com.flash3388.flashlib.robot.scheduling.actions.Actions;
 import com.flash3388.flashlib.robot.scheduling.triggers.Trigger;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -12,29 +13,17 @@ public class BallCountingSystem extends Subsystem {
     private static final int INTAKE_SENSOR_PORT = 0;
     private static final int MAX_BALLS_LEGALLY = 5;
 
-    private final BooleanSupplier feederSupplier;
-    private final BooleanSupplier intakeSupplier;
     private final int maxBalls;
     private int counter;
     private boolean shooting;
 
-    private final Trigger additionTrigger;
-    private final Trigger removalTrigger;
-
     public BallCountingSystem(BooleanSupplier feederSupplier, BooleanSupplier intakeSupplier, int maxBalls) {
-        this.feederSupplier = feederSupplier;
-        this.intakeSupplier = intakeSupplier;
         this.maxBalls = maxBalls;
+
         counter = 0;
         shooting = false;
-
-        additionTrigger = new Trigger();
-        removalTrigger = new Trigger();
-
-        setAdditionAction();
-        setRemovalAction();
-        addAdditionToScheduler();
-        addRemovalToScheduler();
+        Actions.conditional(intakeSupplier, additionAction());
+        Actions.conditional(() -> shooting && !feederSupplier.getAsBoolean(), removalAction());
     }
 
     public static  BallCountingSystem forRobot() {
@@ -57,19 +46,11 @@ public class BallCountingSystem extends Subsystem {
         return counter <= 0;
     }
 
-    private void setAdditionAction() {
-        Actions.instantAction(() -> counter += counter >= maxBalls ? 0 : 1);
+    private Action additionAction() {
+        return Actions.instantAction(() -> counter += counter >= maxBalls ? 0 : 1);
     }
 
-    private void setRemovalAction() {
-        Actions.instantAction(() -> counter -= counter <= 0 ? 0 : 1);
-    }
-
-    private void addAdditionToScheduler() {
-        additionTrigger.addToScheduler(intakeSupplier);
-    }
-
-    private void addRemovalToScheduler() {
-        removalTrigger.addToScheduler(() -> shooting && !feederSupplier.getAsBoolean());
+    private Action removalAction() {
+        return Actions.instantAction(() -> counter -= counter <= 0 ? 0 : 1);
     }
 }
