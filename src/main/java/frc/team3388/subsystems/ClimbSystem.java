2c274567fcc5c0e3ca6d26965a5a4feff5291115
Quacking duck <danielmkhlv@gmail.com>
2020-02-10 04:13:10 +0200

    added indicators to the subsystem

--- src/main/java/frc/team3388/subsystems/ClimbSystem.java
+++ src/main/java/frc/team3388/subsystems/ClimbSystem.java
@@ -1,19 +1,39 @@
 package frc.team3388.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
+
+import java.util.function.BooleanSupplier;
 
 public class ClimbSystem extends ConstantSpeedRotatableSubsystem{
     private static final int CONTROLLER_PORT = 4;
+    private static final int LOW_INDICATOR_PORT = 4;
+    private static final int HIGH_INDICATOR_PORT = 5;
     private static final double SPEED = 1;
 
-    public ClimbSystem(SpeedController controller) {
+    private final BooleanSupplier lowIndicator;
+    private final BooleanSupplier highIndicator;
+
+    public ClimbSystem(SpeedController controller, BooleanSupplier lowIndicator, BooleanSupplier highIndicator) {
         super(controller, SPEED);
+        this.lowIndicator = lowIndicator;
+        this.highIndicator = highIndicator;
     }
 
     public static ClimbSystem forRobot() {
         WPI_TalonSRX liftController = new WPI_TalonSRX(CONTROLLER_PORT);
+        BooleanSupplier lowIndicator = () -> !new DigitalInput(LOW_INDICATOR_PORT).get();
+        BooleanSupplier highIndicator = () -> !new DigitalInput(HIGH_INDICATOR_PORT).get();
 
-        return new ClimbSystem(liftController);
+        return new ClimbSystem(liftController, lowIndicator, highIndicator);
+    }
+
+    public boolean isLow() {
+        return lowIndicator.getAsBoolean();
+    }
+
+    public boolean isHigh() {
+        return highIndicator.getAsBoolean();
     }
 }
