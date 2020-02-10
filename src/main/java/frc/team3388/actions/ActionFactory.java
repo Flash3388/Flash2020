dfca9c936681286d8701c9161a02a3707de778eb
Quacking duck <danielmkhlv@gmail.com>
2020-02-10 04:09:52 +0200

    added the ConstantSpeed... subsystem and removed acitons from action factory

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -1,39 +1,5 @@
 package frc.team3388.actions;
 
-import com.flash3388.flashlib.robot.scheduling.actions.Action;
-import com.flash3388.flashlib.robot.scheduling.actions.Actions;
-import com.flash3388.flashlib.time.Time;
-import frc.team3388.subsystems.ClimbSystem;
-
 public class ActionFactory {
-    public static Action timedHookRaiseAction(ClimbSystem climbSystem) {
-        final Time RAISING_TIME = Time.seconds(1.5);
-        return raiseHookAction(climbSystem).setTimeout(RAISING_TIME);
-    }
 
-    public static Action timedHookLowerAction(ClimbSystem climbSystem) {
-        final Time LOWERING_TIME = Time.seconds(1.5);
-        return lowerHookAction(climbSystem).setTimeout(LOWERING_TIME);
-    }
-
-    public static Action raiseHookAction(ClimbSystem climbSystem) {
-        return Actions.sequential(
-                Actions.runnableAction(climbSystem::raiseHook),
-                Actions.instantAction(climbSystem::stopHook)
-        ).requires(climbSystem);
-    }
-
-    public static Action lowerHookAction(ClimbSystem climbSystem) {
-        return Actions.sequential(
-                Actions.runnableAction(climbSystem::lowerHook),
-                Actions.instantAction(climbSystem::stopHook)
-        ).requires(climbSystem);
-    }
-
-    public static Action liftRobotAction(ClimbSystem climbSystem) {
-        return Actions.sequential(
-                Actions.runnableAction(climbSystem::liftRobot),
-                Actions.instantAction(climbSystem::stopRobotLift)
-        ).requires(climbSystem);
-    }
 }
--- src/main/java/frc/team3388/subsystems/ClimbSystem.java
+++ src/main/java/frc/team3388/subsystems/ClimbSystem.java
@@ -1,47 +1,19 @@
 package frc.team3388.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.flash3388.flashlib.robot.scheduling.Subsystem;
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class ClimbSystem extends Subsystem {
-    private static final int LIFT_CONTROLLER_PORT = 4;
-    private static final int HOOK_CONTROLLER_PORT = 5;
-    private static final double HOOK_LIFT_SPEED = 0.3;
-    private static final double LIFT_SPEED = 1;
+public class ClimbSystem extends ConstantSpeedRotatableSubsystem{
+    private static final int CONTROLLER_PORT = 4;
+    private static final double SPEED = 1;
 
-    private SpeedController liftController;
-    private SpeedController hookController;
-
-    public ClimbSystem(SpeedController liftController , SpeedController hookController) {
-        this.liftController = liftController;
-        this.hookController = hookController;
+    public ClimbSystem(SpeedController controller) {
+        super(controller, SPEED);
     }
 
     public static ClimbSystem forRobot() {
-        WPI_TalonSRX liftController = new WPI_TalonSRX(LIFT_CONTROLLER_PORT);
-        WPI_TalonSRX hookController = new WPI_TalonSRX(HOOK_CONTROLLER_PORT);
+        WPI_TalonSRX liftController = new WPI_TalonSRX(CONTROLLER_PORT);
 
-        return new ClimbSystem(liftController, hookController);
-    }
-
-    public void liftRobot() {
-        liftController.set(LIFT_SPEED);
-    }
-
-    public void raiseHook() {
-        hookController.set(HOOK_LIFT_SPEED);
-    }
-
-    public void lowerHook() {
-        hookController.set(-HOOK_LIFT_SPEED);
-    }
-
-    public void stopRobotLift() {
-        liftController.stopMotor();
-    }
-
-    public void stopHook() {
-        hookController.stopMotor();
+        return new ClimbSystem(liftController);
     }
 }
