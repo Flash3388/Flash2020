58302197f10e7cf4e396d852e37148bc976f8a33
Quacking duck <danielmkhlv@gmail.com>
2020-02-11 21:57:24 +0200

    updated after tests

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -1,10 +1,19 @@
 package frc.team3388.actions;
 
 import com.flash3388.flashlib.robot.scheduling.actions.Action;
+import com.flash3388.flashlib.robot.scheduling.actions.Actions;
+import frc.team3388.subsystems.TurretSystem;
 
 import java.util.function.BooleanSupplier;
 
 public class ActionFactory {
+    public static Action rotateTurretUntil(TurretSystem turretSystem, double target, double speed) {
+        return onCondition(Actions.sequential(
+                Actions.runnableAction(() -> turretSystem.rotate(speed)),
+                Actions.instantAction(turretSystem::stop)), () -> turretSystem.hasReachedAngle(target)
+        ).requires(turretSystem);
+    }
+
     public static Action onCondition(Action action, BooleanSupplier condition) {
         return new ConditionalAction(action, condition);
     }
--- src/main/java/frc/team3388/subsystems/TurretSystem.java
+++ src/main/java/frc/team3388/subsystems/TurretSystem.java
@@ -6,14 +6,17 @@ import com.flash3388.flashlib.robot.control.PidController;
 import com.flash3388.flashlib.robot.motion.Rotatable;
 import com.flash3388.flashlib.robot.scheduling.Subsystem;
 import com.jmath.ExtendedMath;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.SpeedController;
 import frc.team3388.objects.SrxEncoder;
 
 public class TurretSystem extends PreciseRotatableSubsystem {
     private static final int CONTROLLER_PORT = 3;
-    private static final double DEFAULT_MAX_ANGLE = 360;
-    private static final int LARGE_GEAR_TOOTH_COUNT = 100;
-    private static final int LITTLE_GEAR_TOOTH_COUNT = 10;
+    private static final double DEFAULT_MAX_ANGLE = 135;
+    private static final int LARGE_GEAR_TOOTH_COUNT = 110;
+    private static final int LITTLE_GEAR_TOOTH_COUNT = 27;
     private static final double PID_LIMIT = 0.4;
     private static final double DEFAULT_DELTA = 0.5;
 
@@ -22,29 +25,29 @@ public class TurretSystem extends PreciseRotatableSubsystem {
     public TurretSystem(SpeedController controller, PidController pidController, SrxEncoder encoder, double maxAngle) {
         super(new FrcSpeedController(controller), encoder, pidController, PID_LIMIT, DEFAULT_DELTA);
         this.maxAngle = maxAngle;
-
-        pidController.setOutputLimit(PID_LIMIT);
     }
 
-    public TurretSystem forRobot() {
+    public static TurretSystem forRobot() {
         final double kP = 0.1;
         final double kI = 0;
-        final double kD = 0;
+        final double kD = 0.1;
 
         PidController pidController = new PidController(kP, kI, kD, 0);
         WPI_TalonSRX talon = new WPI_TalonSRX(CONTROLLER_PORT);
-        SrxEncoder encoder = new SrxEncoder(CONTROLLER_PORT, LITTLE_GEAR_TOOTH_COUNT/(double)LARGE_GEAR_TOOTH_COUNT);
+        talon.setInverted(true);
+        SrxEncoder encoder = new SrxEncoder(CONTROLLER_PORT, -LITTLE_GEAR_TOOTH_COUNT/(double)LARGE_GEAR_TOOTH_COUNT * 360);
+        encoder.reset();
 
         return new TurretSystem(talon, pidController, encoder, DEFAULT_MAX_ANGLE);
     }
 
     @Override
     public void rotate(double speed) {
-        if(canRotate())
-            super.rotate(speed);
+        speed = canRotate() ? speed : 0;
+        super.rotate(speed);
     }
 
-    private boolean canRotate() {
+    public boolean canRotate() {
         return ExtendedMath.constrained(currentValue(), -maxAngle, maxAngle);
     }
 }
