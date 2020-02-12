b7dd4c79f4f5d5aefe53908d31da4d626673fabd
Quacking duck <danielmkhlv@gmail.com>
2020-02-12 02:58:28 +0200

    added lid actions

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -1,5 +1,6 @@
 package frc.team3388.actions;
 
+import com.flash3388.flashlib.robot.motion.actions.RotateAction;
 import com.flash3388.flashlib.robot.scheduling.actions.Action;
 import com.flash3388.flashlib.robot.scheduling.actions.Actions;
 import frc.team3388.subsystems.ShooterSystem;
@@ -7,10 +8,17 @@ import frc.team3388.subsystems.ShooterSystem;
 import java.util.function.BooleanSupplier;
 
 public class ActionFactory {
-    public static Action interpolateShootAction(ShooterSystem shooterSystem, double distance) {
+    public static Action highInterpolateShootAction(ShooterSystem shooterSystem, double distance) {
         return Actions.sequential(
                 Actions.instantAction(shooterSystem::hideLid),
                 shooterSystem.keepAtAction(shooterSystem.interpolateRpm(distance))
+        );
+    }
+
+    public static Action lowShootAction(ShooterSystem shooterSystem) {
+        return Actions.sequential(
+                Actions.instantAction(shooterSystem::closeLid),
+                new RotateAction(shooterSystem, () -> 0.5)
         );
     }
 
