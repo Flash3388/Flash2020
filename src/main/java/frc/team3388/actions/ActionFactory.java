ba3b04413f4185f3b64a6b10f8ecaf105b9353f7
Quacking duck <danielmkhlv@gmail.com>
2020-02-15 04:13:38 +0200

    integrated vision with turret

--- src/main/java/frc/team3388/actions/ActionFactory.java
+++ src/main/java/frc/team3388/actions/ActionFactory.java
@@ -2,16 +2,20 @@ package frc.team3388.actions;
 
 import com.flash3388.flashlib.robot.scheduling.actions.Action;
 import com.flash3388.flashlib.robot.scheduling.actions.Actions;
+import com.flash3388.flashlib.time.Time;
 import frc.team3388.subsystems.TurretSystem;
+import frc.team3388.subsystems.VisionSystem;
 
 import java.util.function.BooleanSupplier;
 
 public class ActionFactory {
-    public static Action rotateTurretUntil(TurretSystem turretSystem, double target, double speed) {
-        return onCondition(Actions.sequential(
-                Actions.runnableAction(() -> turretSystem.rotate(speed)),
-                Actions.instantAction(turretSystem::stop)), () -> turretSystem.hasReachedAngle(target)
-        ).requires(turretSystem);
+    public static Action rotateTurretByVision(TurretSystem turretSystem, VisionSystem visionSystem) {
+        double initialTurretAngle = turretSystem.currentValue();
+
+        return Actions.sequential(
+                Actions.wait(Time.milliseconds(100)),
+                turretSystem.keepAtAction(() -> initialTurretAngle + visionSystem.alignmentError())
+        ).requires(turretSystem, visionSystem);
     }
 
     public static Action onCondition(Action action, BooleanSupplier condition) {
