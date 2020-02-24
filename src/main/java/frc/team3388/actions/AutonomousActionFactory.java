65590a1df300896b12203abade2923695fcc551c
Quacking duck <danielmkhlv@gmail.com>
2020-02-24 20:25:46 +0200

    added autos

---
+++ src/main/java/frc/team3388/actions/AutonomousActionFactory.java
@@ -0,0 +1,35 @@
+package frc.team3388.actions;
+
+import calculus.splines.parameters.Waypoint;
+import com.flash3388.flashlib.robot.scheduling.actions.Action;
+import com.flash3388.flashlib.robot.scheduling.actions.Actions;
+import com.flash3388.flashlib.time.Clock;
+import com.flash3388.flashlib.time.Time;
+import frc.team3388.subsystems.*;
+
+public class AutonomousActionFactory {
+    public static Action rightSideAutonomous(DriveSystem driveSystem, IntakeSystem intakeSystem, HopperSystem hopperSystem, FeederSystem feederSystem, TurretSystem turretSystem, ShooterSystem shooterSystem, VisionSystem visionSystem, Clock clock) {
+        return Actions.sequential(
+                turretSystem.roughRotateToAction(() -> -20),
+                ActionFactory.visionShootAction(intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem, visionSystem).setTimeout(Time.seconds(4)),
+                Actions.parallel(
+                        ActionFactory.fullIntakeAction(intakeSystem, hopperSystem),
+                        driveFromInitiationLineToControlPanel(driveSystem, clock)
+                ),
+                driveFromControlPanelToInitiationLine(driveSystem, clock),
+                ActionFactory.visionShootAction(intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem, visionSystem).setTimeout(Time.seconds(3))
+        ).requires(intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem, visionSystem);
+    }
+
+    public static Action driveFromInitiationLineToControlPanel(DriveSystem driveSystem, Clock clock) {
+        return driveSystem.autonomousForwardDrive(clock, new Waypoint(), new Waypoint(3, 0, 0));
+    }
+
+    public static Action driveFromControlPanelToInitiationLine(DriveSystem driveSystem, Clock clock) {
+        return driveSystem.autonomousBackwardDrive(clock, new Waypoint(0, 0, Math.toRadians(180)), new Waypoint(-3, 0, Math.toRadians(180)));
+    }
+
+    public static Action driveBackwardsTwoMeters(DriveSystem driveSystem, Clock clock) {
+        return driveSystem.autonomousBackwardDrive(clock, new Waypoint(0, 0, Math.toRadians(180)), new Waypoint(-2, 0, Math.toRadians(180)));
+    }
+}
--- src/main/java/frc/team3388/subsystems/DriveSystem.java
+++ src/main/java/frc/team3388/subsystems/DriveSystem.java
@@ -1,7 +1,10 @@
 package frc.team3388.subsystems;
 
+import calculus.splines.SplineType;
+import calculus.splines.parameters.Waypoint;
 import calculus.trajectories.TankTrajectory;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
 import com.flash3388.flashlib.frc.robot.io.devices.actuators.FrcSpeedController;
 import com.flash3388.flashlib.robot.io.devices.actuators.SpeedController;
 import com.flash3388.flashlib.robot.io.devices.actuators.SpeedControllerGroup;
@@ -25,13 +28,14 @@ public class DriveSystem extends TankDriveSystem implements TankFollower {
     private static final int FRONT_LEFT_CONTROLLER_PORT = 5;
     private static final int REAR_LEFT_CONTROLLER_PORT = 7;
     private static final MotionState DEFAULT_MAX_PARAMETERS = MotionState.frcKitOfPartsParameters(1.5);
+    private static final double DISTANCE_BETWEEN_WHEELS = 0.6;
 
     private final TankTrajectoryControllerFactory controllerFactory;
-    private final Gyro gyro;
+    private final PigeonIMU gyro;
     private final SrxEncoder rightEncoder;
     private final SrxEncoder leftEncoder;
 
-    public DriveSystem(SpeedController frontRight, SpeedController rearRight, SpeedController frontLeft, SpeedController rearLeft, TankTrajectoryControllerFactory controllerFactory, Gyro gyro, SrxEncoder rightEncoder, SrxEncoder leftEncoder) {
+    public DriveSystem(SpeedController frontRight, SpeedController rearRight, SpeedController frontLeft, SpeedController rearLeft, TankTrajectoryControllerFactory controllerFactory, PigeonIMU gyro, SrxEncoder rightEncoder, SrxEncoder leftEncoder) {
         super(new SpeedControllerGroup(frontRight, rearRight), new SpeedControllerGroup(frontLeft, rearLeft));
         this.controllerFactory = controllerFactory;
         this.rightEncoder = rightEncoder;
@@ -41,33 +45,51 @@ public class DriveSystem extends TankDriveSystem implements TankFollower {
 
     public static DriveSystem forRobot() {
         final double CM_TO_M_RATIO = 0.01;
+        final double WHEEL_DIAMETER = 15.24;
 
         WPI_TalonSRX frontRight = new WPI_TalonSRX(FRONT_RIGHT_CONTROLLER_PORT);
         WPI_TalonSRX rearRight = new WPI_TalonSRX(REAR_RIGHT_CONTROLLER_PORT);
         WPI_TalonSRX frontLeft = new WPI_TalonSRX(FRONT_LEFT_CONTROLLER_PORT); frontLeft.setInverted(true);
         WPI_TalonSRX rearLeft = new WPI_TalonSRX(REAR_LEFT_CONTROLLER_PORT); rearLeft.setInverted(true);
+        PigeonIMU gyro = new PigeonIMU(frontLeft);
         TankTrajectoryControllerFactory controllerFactory = createControllerFactory();
 
         return new DriveSystem(
                 new FrcSpeedController(frontRight), new FrcSpeedController(rearRight),
                 new FrcSpeedController(frontLeft), new FrcSpeedController(rearLeft),
-                controllerFactory, null, new SrxEncoder(frontRight, CM_TO_M_RATIO), new SrxEncoder(frontLeft, CM_TO_M_RATIO));
+                controllerFactory, gyro, new SrxEncoder(frontRight, CM_TO_M_RATIO * WHEEL_DIAMETER), new SrxEncoder(frontLeft, CM_TO_M_RATIO * WHEEL_DIAMETER));
+    }
+
+    public Action autonomousForwardDrive(Clock clock, Waypoint... path) {
+        return autonomousForwardDrive(new TankTrajectory(SplineType.CUBIC_HERMITE, DISTANCE_BETWEEN_WHEELS, path), clock);
+    }
+
+    public Action autonomousBackwardDrive(Clock clock, Waypoint... path) {
+        return autonomousBackwardDrive(new TankTrajectory(SplineType.CUBIC_HERMITE, DISTANCE_BETWEEN_WHEELS, path), clock);
     }
 
     public Action autonomousForwardDrive(TankTrajectory trajectory, Clock clock) {
-        return new TankFollowerAction(this, controllerFactory.create(trajectory, DEFAULT_MAX_PARAMETERS, RobotController.getBatteryVoltage(), Direction.FORWARD), clock);
+        return new TankFollowerAction(this, controllerFactory.create(trajectory, DEFAULT_MAX_PARAMETERS, RobotController.getBatteryVoltage(), Direction.FORWARD), clock).requires(this);
     }
 
     public Action autonomousBackwardDrive(TankTrajectory trajectory, Clock clock) {
-        return new TankFollowerAction(this, controllerFactory.create(trajectory, DEFAULT_MAX_PARAMETERS.mul(-1), RobotController.getBatteryVoltage(), Direction.BACKWARD), clock);
+        return new TankFollowerAction(this, controllerFactory.create(trajectory, DEFAULT_MAX_PARAMETERS.mul(-1), RobotController.getBatteryVoltage(), Direction.BACKWARD), clock).requires(this);
     }
 
     public Action autonomousDrive(TankTrajectory trajectory, MotionState max, double maxVoltage, Direction direction, Clock clock) {
-        return new TankFollowerAction(this, controllerFactory.create(trajectory, max, maxVoltage, direction), clock);
+        return new TankFollowerAction(this, controllerFactory.create(trajectory, max, maxVoltage, direction), clock).requires(this);
     }
 
     public void calibrateGyro() {
-        gyro.calibrate();
+        gyro.enterCalibrationMode(PigeonIMU.CalibrationMode.BootTareGyroAccel);
+    }
+
+    @Override
+    public void resetMeasuringDevices() {
+        leftEncoder.reset();
+        rightEncoder.reset();
+        gyro.setFusedHeading(0);
+        gyro.setYaw(0);
     }
 
     @Override
@@ -82,7 +104,7 @@ public class DriveSystem extends TankDriveSystem implements TankFollower {
 
     @Override
     public double angle() {
-        return gyro.getAngle();
+        return gyro.getFusedHeading();
     }
 
     private static  TankTrajectoryControllerFactory createControllerFactory() {
