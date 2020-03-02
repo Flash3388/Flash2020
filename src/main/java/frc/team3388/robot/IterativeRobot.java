aa4c2b738ed4dfe376f6dad500469ea8f3bc66fb
Quacking duck <danielmkhlv@gmail.com>
2020-03-02 21:40:33 +0200

    made tests better

--- src/main/java/frc/team3388/robot/IterativeRobot.java
+++ src/main/java/frc/team3388/robot/IterativeRobot.java
@@ -6,21 +6,14 @@ import com.flash3388.flashlib.robot.Robot;
 import com.flash3388.flashlib.robot.motion.actions.RotateAction;
 import com.flash3388.flashlib.robot.scheduling.actions.Action;
 import com.flash3388.flashlib.robot.scheduling.actions.Actions;
-import com.flash3388.flashlib.time.Time;
 import edu.wpi.first.networktables.EntryListenerFlags;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.team3388.actions.ActionFactory;
 import frc.team3388.actions.AutonomousActionFactory;
-import frc.team3388.actions.RotateTurretUntilSolidTargetAction;
-import frc.team3388.objects.NetworkDoubleProperty;
-import frc.team3388.objects.NetworkDoubleSupplier;
 import frc.team3388.subsystems.*;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class IterativeRobot implements IterativeRobotInterface {
@@ -37,16 +30,7 @@ public class IterativeRobot implements IterativeRobotInterface {
         VisionSystem visionSystem = VisionSystem.forRobot();
         autoAction = AutonomousActionFactory.genericAuto(driveSystem, intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem, visionSystem, robot.getClock());
 
-        Queue<Testable> testables = new ArrayDeque<>(Arrays.asList(driveSystem, intakeSystem, hopperSystem, feederSystem, shooterSystem, turretSystem, climbSystem));
-        List<Action> testActions = testables.stream()
-                .map(Testable::tests)
-                .collect(Collectors.toList());
-
-        NetworkTableInstance.getDefault().getTable("test").getEntry("shouldTest").addListener(entryNotification -> {
-            if(entryNotification.value.getBoolean())
-                testActions.forEach(Action::start);
-            entryNotification.getEntry().setBoolean(false);
-        }, EntryListenerFlags.kUpdate);
+        setupTests(driveSystem, intakeSystem, hopperSystem, feederSystem, shooterSystem, turretSystem, climbSystem);
 
         Joystick systemController = new Joystick(0);
         Joystick right = new Joystick(1);
@@ -60,7 +44,7 @@ public class IterativeRobot implements IterativeRobotInterface {
         right.getButton(0).whileHeld(ActionFactory.initiationLineShootAction(intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem));
         right.getButton(1).whenPressed(ActionFactory.switchCamAction(visionSystem));
         right.getButton(2).whileHeld(new RotateAction(hopperSystem, () -> -0.8));
-        left.getButton(2).whileHeld(ActionFactory.fullIntakeAction(intakeSystem, hopperSystem));
+
         systemController.getButton(0).whileHeld(ActionFactory.visionShootAction(intakeSystem, hopperSystem, feederSystem, turretSystem, shooterSystem, visionSystem));
         systemController.getButton(1).whileHeld(ActionFactory.fullEatAction(intakeSystem, hopperSystem, robot.getClock()));
         systemController.getButton(2).whenPressed(ActionFactory.foldIntakeAction(intakeSystem));
@@ -106,5 +90,17 @@ public class IterativeRobot implements IterativeRobotInterface {
 
     @Override
     public void robotPeriodic() {
+    }
+
+    private static void setupTests(Testable... testables) {
+        List<Action> testActions = Arrays.stream(testables)
+                .map(Testable::tests)
+                .collect(Collectors.toList());
+
+        NetworkTableInstance.getDefault().getTable("test").getEntry("shouldTest").addListener(entryNotification -> {
+            if(entryNotification.value.getBoolean())
+                testActions.forEach(Action::start);
+            entryNotification.getEntry().setBoolean(false);
+        }, EntryListenerFlags.kUpdate);
     }
 }
